e5a74d1bbdcf64a79405ee90aded582770c1f86f
cyxabima <ukashaanwerali@gmail.com>
2025-03-08 11:42:52 +0500

    wildcard cors added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth, vote
 from . import models
@@ -8,6 +9,14 @@ from .routers import posts, users
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
